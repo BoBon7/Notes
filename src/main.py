ac012f8eefc601c618e73344baf43cadca3bedc2
BoBon7 <mbruveris@mail.ru>
2024-09-26 09:59:37 +0300

    Revert "added function for changing note"
    This reverts commit 22c7a04449822b2876b237d06bae34db4159d0f7.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.exc import IntegrityError
 
 from database import get_async_session
 from notes.schemas import *
-from sqlalchemy import insert, select, update, values
+from sqlalchemy import insert, select
 from fastapi import FastAPI, Depends, HTTPException
 from datetime import datetime
 
@@ -28,7 +28,7 @@ async def add_note(note: Note, session: AsyncSession = Depends(get_async_session
         await session.execute(stmt)
         await session.commit()
         return {"status": "success", "data": f"Added {note.note_title} for user {note.user_id}", "details": None}
-    except IntegrityError:
+    except IntegrityError as ex:
         raise HTTPException(status_code=410, detail={
             "status": "error",
             "data": None,
@@ -39,8 +39,7 @@ async def add_note(note: Note, session: AsyncSession = Depends(get_async_session
 @app.post("/get_note")  # по-хорошему переделать на get, но при этом не безопасно так передавать user_id
 async def get_note(user_id: int, note_title: str, session: AsyncSession = Depends(get_async_session)):
     try:
-        query = select(notes_table.c.note_title, notes_table.c.note_text).where(
-            notes_table.c.user_id == user_id and notes_table.c.note_title == note_title)
+        query = select(notes_table.c.note_title, notes_table.c.note_text).where(notes_table.c.user_id == user_id and notes_table.c.note_title == note_title)
         result = await session.execute(query)
         return {
             "status": "success",
@@ -70,28 +69,4 @@ async def get_all(user_id: int, session: AsyncSession = Depends(get_async_sessio
             "status": "error",
             "data": None,
             "details": None
-        })
-
-
-@app.patch("/change_note")
-async def change_note(user_id: int, note_title: str, note_text: str, new_title: str = None,
-                      session: AsyncSession = Depends(get_async_session)):
-    query = ""
-    try:
-        if note_title:
-            query = (
-                update(notes_table).where(notes_table.c.user_id == user_id and notes_table.c.note_title == note_title)
-                .values(note_title=new_title, note_text=note_text, updated_at=datetime.utcnow()))
-        else:
-            query = (
-                update(notes_table).where(notes_table.c.user_id == user_id and notes_table.c.note_title == note_title)
-                .values(note_text=note_text, updated_at=datetime.utcnow()))
-        await session.execute(query)
-        await session.commit()
-        return {"status": "success", "data": f"Changed", "details": None}
-    except IntegrityError:
-        raise HTTPException(status_code=500, detail={
-            "status": "error",
-            "data": None,
-            "details": "Not unique note title for this user"
         })
